0709165c1c566049aa61f3a1eba5ef7eaddd66f3
LeThanhNhan <lnhan8132@gmail.com>
2025-09-05 23:22:20 +0700

    cập nhật giao diện

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -11,6 +11,10 @@ export default defineConfig({
         target: 'http://127.0.0.1:5000',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+      '/admin': {
+        target: 'http://127.0.0.1:5000',
+        changeOrigin: true,
       }
     }
   }
